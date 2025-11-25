7af8ccdb4b3f282ed64866ea8f0cd5fe33c395d1
Suraj Kumar <suraj71308kumar@gmail.com>
2025-11-25 13:16:11 +0530

    sinup and signin page done with backend api's integration

--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const primaryColor = "#ff4d2d";
+  const hoverColor = "#e64323";
+  const bgColor = "#fff9f6";
+  const borderColor = "#ddd";
 
-export default SignIn
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const serverUrl = "http://localhost:3000";
+  const handleSignIn = async () => {
+    try {
+      const result = await axios.post(
+        `${serverUrl}/api/auth/signin`,
+        { email, password },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("result data from signin: ", result);
+      // reset input fields
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log("signup error from fetching signup api", error);
+    }
+  };
+
+  return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center p-4"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div
+        className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border`}
+        style={{ border: `1px solid ${borderColor}` }}
+      >
+        <h1
+          className={`text-3xl font-bold mb-2`}
+          style={{ color: primaryColor }}
+        >
+          SnackSprint
+        </h1>
+        <p className="text-gray-600 mb-8">
+          Create your accout to get started with delicious food deliveries
+        </p>
+
+        {/* Email */}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Email
+          </label>
+          <input
+            type="text"
+            required
+            value={email}
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              required
+              value={password}
+              className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-2.5 text-gray-600 cursor-pointer"
+            >
+              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+        </div>
+
+        <div className={`text-right font-medium text-[#ff4d2d] mb-2 cursor-pointer`}>
+          Forget password
+        </div>
+
+        <button
+          className={`mb-4 w-full rounded-md py-1.5 hover:bg-[#e64323] bg-[#ff4d2d] text-white font-medium cursor-pointer`}
+          onClick={handleSignIn}
+        >
+          Sign in
+        </button>
+
+        <button
+          className={`mt-2 w-full rounded-md cursor-pointer flex items-center justify-center gap-2 border 
+            py-1.5 border-gray-300 shadow transition duration-200 hover:bg-gray-200 font-serif`}
+        >
+          <FcGoogle size={18} />
+          <span>Sign In with google</span>
+        </button>
+
+        <p className="mt-2 text-center">
+          want to create new account ?{" "}
+          <Link to={"/signup"}>
+            <span className="text-blue-600">Signup Now</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,10 +1,186 @@
-
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const primaryColor = "#ff4d2d";
+  const hoverColor = "#e64323";
+  const bgColor = "#fff9f6";
+  const borderColor = "#ddd";
 
-export default SignUp
+  const [showPassword, setShowPassword] = useState(false);
+  const [role, setRole] = useState("user");
+
+  const [fullname, setFullname] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+
+  const serverUrl = "http://localhost:3000";
+  const handleSignup = async () => {
+    try {
+      const result = await axios.post(
+        `${serverUrl}/api/auth/signup`,
+        { fullname, email, password, mobile, role },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("result data from signup: ", result);
+      // reset input fields
+      setFullname("");
+      setEmail("");
+      setPassword("");
+      setMobile("");
+      setRole("");
+    } catch (error) {
+      console.log("signup error from fetching signup api", error);
+    }
+  };
+
+  return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center p-4"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div
+        className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border`}
+        style={{ border: `1px solid ${borderColor}` }}
+      >
+        <h1
+          className={`text-3xl font-bold mb-2`}
+          style={{ color: primaryColor }}
+        >
+          SnackSprint
+        </h1>
+        <p className="text-gray-600 mb-8">
+          Create your accout to get started with delicious food deliveries
+        </p>
+
+        {/* Full Name */}
+        <div className="mb-4">
+          <label
+            htmlFor="fullName"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            FullName
+          </label>
+          <input
+            type="text"
+            value={fullname} required
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your fullname"
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
+          />
+        </div>
+        {/* Email */}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Email
+          </label>
+          <input
+            type="text" required
+            value={email}
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* Mobile */}
+        <div className="mb-4">
+          <label
+            htmlFor="mobile"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Mobile Number
+          </label>
+          <input
+            type="text" required
+            value={mobile}
+            className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+            placeholder="Enter your mobile number"
+            onChange={(e) => setMobile(e.target.value)}
+          />
+        </div>
+        {/* Password */}
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"} required
+              value={password}
+              className="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:border-orange-500"
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-2.5 text-gray-600 cursor-pointer"
+            >
+              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+        </div>
+        {/* Role */}
+        <div className="mb-4">
+          <label
+            htmlFor="role"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Role
+          </label>
+          <div className="flex items-center justify-between gap-4 w-full">
+            {["user", "owner", "deliveryboy"].map((r) => (
+              <button
+                key={r}
+                className="px-7 border rounded-md py-1.5 cursor-pointer text-center font-medium transition-colors"
+                onClick={() => setRole(r)}
+                style={
+                  role == r
+                    ? { backgroundColor: primaryColor, color: "white" }
+                    : { border: `1px solid ${primaryColor}`, color: "#333" }
+                }
+              >
+                {r}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button
+          className={`mb-4 w-full rounded-md py-1.5 hover:bg-[#e64323] bg-[#ff4d2d] text-white font-medium cursor-pointer`}
+          onClick={handleSignup}
+        >
+          Sign Up
+        </button>
+        <button
+          className={`mt-2 w-full rounded-md cursor-pointer flex items-center justify-center gap-2 border 
+            py-1.5 border-gray-300 shadow transition duration-200 hover:bg-gray-200 font-serif`}
+        >
+          <FcGoogle size={18} />
+          <span>Sign Up with google</span>
+        </button>
+        <p className="mt-2 text-center">
+          Already have an account ?{" "}
+          <Link to={"/signin"}>
+            <span className="text-blue-600">Signin</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
